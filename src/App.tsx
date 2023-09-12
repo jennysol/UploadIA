fcc8e71541209fe476860d4747a42f2260b1e25a
jennysol <jennyssoliver@gmail.com>
2023-09-12 16:58:09 -0300

    feat: add first section to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Button } from "./components/ui/button";
 import { Github } from "lucide-react";
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">Upload.IA</h1>
 
         <div className="flex items-center gap-3">
-          <span className="text-small text-muted-foreground">
+          <span className="text-sm text-muted-foreground">
             Desenvolvido com 💜
           </span>
 
@@ -21,6 +22,29 @@ export function App() {
           </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              placeholder="Inclua o prompt para IA..."
+              className="resize-none p-4 leading-relaxed"
+            />
+            <Textarea
+              placeholder="Resultado gerado pela IA..."
+              className="resize-none p-4 leading-relaxed"
+              readOnly
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da descrição do vídeo selecionado
+          </p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
     </div>
   );
 }
