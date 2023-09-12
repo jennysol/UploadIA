9c61c6ea49dee31473b5ef5e10e6727fdb75bab6
jennysol <jennyssoliver@gmail.com>
2023-09-12 11:37:17 -0300

    feat: add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import "./App.css";
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <>
-      <h1>Hi Upload IA</h1>
-    </>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">Upload.IA</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-small text-muted-foreground">
+            Desenvolvido com 💜
+          </span>
+
+          <Separator orientation="vertical" className="h-6"></Separator>
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
